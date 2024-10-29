6c172db6cb0b47a91235cd7aab66afee712cf7c5
dmitry416 <93573566+dmitry416@users.noreply.github.com>
2024-10-29 18:47:46 +1000

    Auth + game base

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def game(request):
-    return HttpResponse('<h1>GAME!!!</h1>')
+def auth(request):
+    return HttpResponse('<h1>AUTH!!!</h1>')
