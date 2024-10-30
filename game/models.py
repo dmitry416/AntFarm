4d9e750e60231735779d1bf91f1a7688b1ab9e95
Simankov Alexandr <148748481+Fotlex@users.noreply.github.com>
2024-10-30 15:02:32 +1000

    User model beta version

--- game/models.py
+++ game/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Ant(models.Model):
+    pass
+
+class Chest(models.Model):
+    pass
+
+class User(models.Model):
+    user_id = models.BigIntegerField(primary_key=True)
+    user_name = models.CharField(max_length=32)
+    chest = models.OneToOneField(Chest, on_delete=models.CASCADE)
+    chest_open_time = models.DateTimeField(null=True)
+    boss_date = models.DateField(null=True)
+
+class UserAnts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ant = models.ForeignKey(Ant, on_delete=models.CASCADE)
+    count = models.IntegerField(default=-1)
+
+
