85af7129094251e26d9ef4b6a89b6c68a13b9c8c
Simankov Alexandr <148748481+Fotlex@users.noreply.github.com>
2024-10-31 20:59:48 +1000

    fixed chest

--- game/models.py
+++ game/models.py
@@ -22,7 +22,7 @@ class Boss(models.Model):
 class User(models.Model):
     user_id = models.BigIntegerField(primary_key=True)
     user_name = models.CharField(max_length=32)
-    chest = models.OneToOneField(Chest, on_delete=models.CASCADE)
+    chest = models.ForeignKey(Chest, on_delete=models.CASCADE)
     chest_open_time = models.DateTimeField(null=True)
     boss_date = models.DateField(null=True)
 
