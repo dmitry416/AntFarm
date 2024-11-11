81e0877ca3eaeee8c3f9e47ec5214da7f9c37da8
verhovv <vladimirverhov@yandex.ru>
2024-11-11 19:24:05 +1000

    new models (feat Pasha)
     On branch ProcessingRequests
     Your branch and 'origin/ProcessingRequests' have diverged,
     and have 1 and 1 different commits each, respectively.
    
    Changes to be committed:
    	new file:   game/migrations/0004_item_remove_ant_chances_remove_chest_chances_and_more.py
    	new file:   game/migrations/0005_alter_user_chest.py
    	modified:   game/models.py

--- game/models.py
+++ game/models.py
@@ -1,18 +1,32 @@
 from django.db import models
-from django.template.defaultfilters import default
+
+
+class Item(models.Model):
+    path_to_image = models.CharField(max_length=255)
+    middle_cost = models.IntegerField(default=1)
+    cost = models.IntegerField(default=1)
 
 
 class Ant(models.Model):
     path_to_image = models.CharField(max_length=255)
-    chances = models.FloatField(default)
+    minimal_cost = models.IntegerField()
 
-    minimal_cost = models.IntegerField(default=0)
+
+class AntItemChance(models.Model):
+    ant = models.ForeignKey(Ant, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    weight = models.IntegerField()
 
 
 class Chest(models.Model):
     path_to_image = models.CharField(max_length=255)
-    chances = models.FloatField()
     cost = models.IntegerField()
+
+
+class ChestAntChance(models.Model):
+    chest = models.ForeignKey(Chest, on_delete=models.CASCADE)
+    ant = models.ForeignKey(Ant, on_delete=models.CASCADE)
+    weight = models.IntegerField()
 
 
 class Boss(models.Model):
@@ -25,7 +39,7 @@ class User(models.Model):
     name = models.CharField(max_length=32)
     image_url = models.CharField(max_length=255, null=True)
 
-    chest = models.ForeignKey(Chest, on_delete=models.CASCADE, default=None, null=True)
+    chest = models.ForeignKey(Chest, on_delete=models.SET_NULL, null=True)
     chest_open_time = models.DateTimeField(null=True)
 
     ant_count = models.IntegerField(default=0)
@@ -38,3 +52,16 @@ class UserAnts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ant = models.ForeignKey(Ant, on_delete=models.CASCADE)
     count = models.IntegerField(default=-1)
+    is_sent = models.BooleanField(default=False)
+    return_datetime = models.DateTimeField(null=True)
+
+
+class UserItems(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+
+class ServerState(models.Model):
+    current_boss = models.ForeignKey(Boss, on_delete=models.SET_NULL, null=True)
+    boss_update_time = models.DateTimeField(null=True)
