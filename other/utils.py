08b9a10e15f18dc9cbf6822aff360a4a040a674e
verhovv <vladimirverhov@yandex.ru>
2024-11-13 00:07:24 +1000

    auth time checking

--- other/utils.py
+++ other/utils.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+
+
 def is_tg_hash_valid(params: dict) -> bool:
     from config import BOT_TOKEN
     import hashlib
@@ -9,4 +12,8 @@ def is_tg_hash_valid(params: dict) -> bool:
     secret_key = hashlib.sha256(BOT_TOKEN.encode()).digest()
     hmac_hash = hmac.new(secret_key, msg=check_string.encode(), digestmod=hashlib.sha256).hexdigest()
 
-    return hmac_hash == tg_hash
+    def check_auth_time() -> bool:
+        auth_time = int(params['auth_date'])
+        return timezone.now().timestamp() - auth_time < 60 * 2
+
+    return hmac_hash == tg_hash and check_auth_time()
